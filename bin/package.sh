f4bb59bc4e25757e51699b0dd167fb4010aaf567
Todd Bryan <todd.r.bryan@gmail.com>
2020-09-18 15:12:41 -0700

    Deploy GDAL and OGR cmdline binaries too

--- bin/package.sh
+++ bin/package.sh
@@ -7,10 +7,15 @@ echo Creating deploy package
 
 # make deployment directory and add lambda handler
 mkdir -p $DEPLOY_DIR/lib
+mkdir -p $DEPLOY_DIR/bin
 
 # copy libs
 cp -P ${PREFIX}/lib/*.so* $DEPLOY_DIR/lib/
 cp -P ${PREFIX}/lib64/libjpeg*.so* $DEPLOY_DIR/lib/
+
+# copy binaries
+cp -P ${PREFIX}/bin/gdal* $DEPLOY_DIR/bin/
+cp -P ${PREFIX}/bin/ogr* $DEPLOY_DIR/bin/
 
 strip $DEPLOY_DIR/lib/* || true
 
