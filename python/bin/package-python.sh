0d5c6c26c06c0c96601155c854bf82ab6f4e27d8
Todd Bryan <todd.r.bryan@gmail.com>
2020-09-18 15:13:28 -0700

    Deploy Python commands alongside libs

--- python/bin/package-python.sh
+++ python/bin/package-python.sh
@@ -9,18 +9,26 @@ MAJOR=${PYVERSION%%.*}
 MINOR=${PYVERSION#*.}
 PYVER=${PYVERSION%%.*}.${MINOR%%.*}
 PYPATH=/root/.pyenv/versions/$PYVERSION/lib/python${PYVER}/site-packages/
+BINPATH=/root/.pyenv/versions/$PYVERSION/bin/
 
 echo Creating deploy package for Python $PYVERSION
 
 EXCLUDE="boto3* botocore* pip* docutils* *.pyc setuptools* wheel* coverage* testfixtures* mock* *.egg-info *.dist-info __pycache__ easy_install.py"
+BINEXCLUDE="2to3* easy_install* f2py* idle* *.pyc *.egg-info *.dist-info __pycache__ easy_install.py"
 
 EXCLUDES=()
 for E in ${EXCLUDE}
 do
     EXCLUDES+=("--exclude ${E} ")
 done
+BINEXCLUDES=()
+for E in ${BINEXCLUDE}
+do
+    BINEXCLUDES+=("--exclude ${E} ")
+done
 
 rsync -ax $PYPATH/ $DEPLOY_DIR/ ${EXCLUDES[@]}
+rsync -ax $BINPATH/ $DEPLOY_DIR/ ${BINEXCLUDES[@]}
 
 # prepare dir for lambda layer deployment - https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html#configuration-layers-path
 cp -r $DEPLOY_DIR ./python
